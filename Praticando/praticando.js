36f57f0ba1c2d897b104817bd06b9ce4aafb4797
WalaceRamosDev <contatowalace.dev@gmail.com>
2023-12-14 21:31:32 -0300

    pratice

--- Praticando/praticando.js
+++ Praticando/praticando.js
@@ -1,4 +1,4 @@
-var data = window.prompt('Digite o ano em que está agora')
-var idade = window.prompt('Digite a sua idade')
-var res = data - idade
-window.alert (`A sua idade atual é ${res}`)
+var atual = window.prompt('Em que ano estamos agora ?')
+var nasc = window.prompt('Diga pra mim, em que ano você nasceu ?')
+var res = atual - nasc
+window.alert(`A sua idade atual é de ${res} anos. Parabéns !`)
