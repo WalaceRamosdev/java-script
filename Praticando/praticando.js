f8c1797bac11c2cc6f17a09d61d439e38d344a9f
WalaceRamosDev <contatowalace.dev@gmail.com>
2023-12-14 21:25:58 -0300

    js

--- Praticando/praticando.js
+++ Praticando/praticando.js
@@ -1,2 +1,4 @@
-var nome = window.prompt('Digite seu nome...')
-window.alert(`Que bom que está de volta ${nome}`)
+var data = window.prompt('Digite o ano em que está agora')
+var idade = window.prompt('Digite a sua idade')
+var res = data - idade
+window.alert (`A sua idade atual é ${res}`)
