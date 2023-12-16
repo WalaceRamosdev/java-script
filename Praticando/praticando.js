a59abed5f06d344eb9b0aeed5d35a6c6bcdb228a
WalaceRamosDev <contatowalace.dev@gmail.com>
2023-12-16 20:51:37 -0300

    praticando

--- Praticando/praticando.js
+++ Praticando/praticando.js
@@ -1,4 +1,10 @@
-var atual = window.prompt('Em que ano estamos agora ?')
-var nasc = window.prompt('Diga pra mim, em que ano você nasceu ?')
-var res = atual - nasc
-window.alert(`A sua idade atual é de ${res} anos. Parabéns !`)
+window.alert('Olá ! Pelo visto você foi selecionado...clique em ok !')
+
+var nome = window.prompt('Me diga seu nome :')
+window.alert(`Seja bem vindo ${nome}, logo em seguida, vamos precisar de mais algumas informações.`)
+
+var id = window.prompt('Digite sua idade: ')
+var nasc = window.prompt('Agora diga aonde você nasceu:')
+var work = window.prompt('Trabalha com o que atualmente?')
+
+window.alert(`Que legal! É ótima saber que alguém com sua idade de ${id} anos se interessou pela vaga, vi também que você nasceu em ${nasc}, temos vários colaboradores conterraneos com você, e sei também que ter a profissão de ${work} pode ser nada fácil !`)
