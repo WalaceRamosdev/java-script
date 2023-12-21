3b0ac7a0c63514403940f3b058a341378d33793f
WalaceRamosDev <contatowalace.dev@gmail.com>
2023-12-20 21:55:51 -0300

    js

--- Módulo D - Condições em JavaScript/condicoes.js
+++ Módulo D - Condições em JavaScript/condicoes.js
@@ -1,1 +1,7 @@
-console.log('O console funcionou corretamente')
+var vel = 61
+console.log(`A velocidade do seu carro é de ${vel} Km/h`)
+if (vel > 60){
+    console.log('Você está acima da velocidade permitida. MUTADO!')
+}
+
+console.log('Dirija sempre com o uso do cinto de seguraça')
