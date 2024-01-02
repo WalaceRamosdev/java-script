4d185e95b0ee643e71cfcf7f30c0aaf1aa55325c
WalaceRamosDev <contatowalace.dev@gmail.com>
2024-01-01 21:22:00 -0300

    praticando

--- Módulo D - Condições em JavaScript/nacional.js
+++ Módulo D - Condições em JavaScript/nacional.js
@@ -1,4 +1,8 @@
 function descubrir() {
-    var local = window.document.getElementById('pais')
-    
+    var loc = window.document.getElementById('pais')
+    var res = window.document.querySelector('div#res')
+    var nac = 
+    if (pais = Brasil) {
+    res.innerHTML = `<p>Você é <strong>Brasileiro</strong></p>`
+    }
 }
