f0248ecf5c627d2108c3c67c1b54066a59e40450
WalaceRamosDev <contatowalace.dev@gmail.com>
2024-01-01 21:03:02 -0300

    praticando

--- Módulo D - Condições em JavaScript/ex001.js
+++ Módulo D - Condições em JavaScript/ex001.js
@@ -1,10 +1,9 @@
 var pais = 'Brasil'
 console.log(`Vivendo em ${pais} aproveitando o melhor da vida!`)
-if('pais == Brasil'){
-    console.log('Vocé Brasileiro')
-
+if(pais != 'Brasil'){
+    console.log('Vocé Estrangeiro')
 } else {
-    console.log('Você é estrangeiro')
+    console.log('Você é Brasileiro')
 }
 
 //Último commit de 2023
