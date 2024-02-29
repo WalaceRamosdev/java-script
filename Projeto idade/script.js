d61ffdf546570912e030f5c6a0c9929bd09d0eeb
WalaceRamosDev <contatowalace.dev@gmail.com>
2024-02-29 06:44:57 -0300

    projeto idade

---
+++ Projeto idade/script.js
@@ -0,0 +1,21 @@
+function carregar() {
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('img')
+var data = new Date()
+var hora = data.getHours()
+msg.innerHTML = `Agora são ${hora} horas.`
+
+
+
+if (hora >= 0 && hora <12) {
+    img.src = 'imagens001/foto01-manha.png'
+    document.body.style.background = '#e2cd9f'
+} else if (hora >= 12 && hora < 18) {
+    img.src = 'imagens001/foto02-tarde.png'
+    document.body.style.background = '#b9846f'
+} else {
+    img.src = 'imagens001/foto03-noite.png'
+    document.body.style.background = '#515154'
+}
+
+}
